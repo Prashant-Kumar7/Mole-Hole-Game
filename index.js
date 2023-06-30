98328c107d62fba6c5d382378e9619f20537eab7
Prashant-Kumar7 <133366828+Prashant-Kumar7@users.noreply.github.com>
2023-06-30 20:47:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,14 +26,20 @@ var score = 0;
 
 $(document).click(function(event){
     var l = $(event.target).attr("class");
+    var colString = l.slice(14,15);
+    var rowString = l.slice(8,9);
     if(l.length > 21){
+        $(".row-" + rowString +".col-"+ colString).removeClass("mole");
+        var smack = new Audio("audio/hard-slap-46388.mp3");
+        smack.play();
         score=score+10;
         document.querySelector(".live-score").innerHTML = "+" +score + " Points";
         console.log("YOU HIT THE TARGET")
     } else{
+        var whoosh = new Audio("audio/whoosh-6316.mp3");
+        whoosh.play();
         console.log("YOU MISSED THE TARGET")
     }
 })
 document.querySelector(".live-score").innerHTML = score + " Points";
 document.querySelector(".live-timer").innerHTML =  timer + " seconds";
-
